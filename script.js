79cb74bb35d1fd78a508b1c6709c30181efb3211
MattsDevLab <116913535+MattsDevLab@users.noreply.github.com>
2025-11-07 16:35:48 +0000

    Add an active class on hover to each grid item

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ const grid = document.querySelector("main");
 
 for(let i = 1; i <=256; i++){
    let gridItem = document.createElement("div");
-
    grid.appendChild(gridItem);
 }
+
+let gridItem = document.querySelectorAll("div");
+
+gridItem.forEach((item) =>{
+    item.addEventListener('mouseover', () =>{
+        item.classList.add("grid-item--active");
+    });
+})
+
+
+
+
 
 console.log("Hellow");
