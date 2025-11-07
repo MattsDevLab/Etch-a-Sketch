3aec7d512771135b6fbfb29ea96eed654613a710
MattsDevLab <116913535+MattsDevLab@users.noreply.github.com>
2025-11-07 16:21:25 +0000

    Set up 16 by 16 grid blocks

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hellow");
+const grid = document.querySelector("main");
+
+for(let i = 1; i <=256; i++){
+   let gridItem = document.createElement("div");
+
+   grid.appendChild(gridItem);
+}
+
+console.log("Hellow");
